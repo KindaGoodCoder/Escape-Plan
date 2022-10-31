84241f84b3c6f444dee8350d58a530a6944f618f
Goodman <arafezazgaminglegend@gmail.com>
2022-10-31 18:28:39 +1100

    Still working on it

--- escapeplan.lua
+++ escapeplan.lua
@@ -1,22 +1,23 @@
-exit1 = 0
+gateb = 0
 
 function OnScriptLoaded() print("EscapePlan"); return -1 end
 
 function OnServerStart()
     for i = 1, 10 do createfakeplayer(i) end
     escapecoords()
-    print(exit1[1])
+    print(gateb[1])
     return -1
 end
 
 function escapecoords()
     local entitypointers = {}    
-    local findcoords = function(index) entity = getroomobjectentity(i,index); return entity,{entityx(entity),entityy(entity),entityz(entity)} end
-        local select = {
-            exit1 = function() entitypointers[1], exit1, entitypointers[2], escape1 = findcoords(26),findcoords(27) end,
-            gatea = function() entitypointers[3], exit2, entitypointers[4], escape2 = findcoords(27),findcoords(11) end
-        
+    local findcoords = function(room,index) entity = getroomobjectentity(room,index); return entity,{entityx(entity),entityy(entity),entityz(entity)} end
+    local select = {
+        ["exit1"] = function(i) entitypointers[1], gateb = findcoords(i,26) end,
+        gatea = function() entitypointers[3], exit2, entitypointers[4], escape2 = findcoords(27),findcoords(11) end
+    }
     for i = 1, 70 do
-        if type(select[getroomname(i)]) == "function" then select[getroomname(i)]() end
+        print(getroomname(i))
+        if type(select[getroomname(i)]) == "function" then select[getroomname(i)](i) end
     end
 end
