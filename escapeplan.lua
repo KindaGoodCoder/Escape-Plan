f2ef780f44089022079d19a2db0999308def1d01
Goodman <arafezazgaminglegend@gmail.com>
2022-11-01 17:55:10 +1100

    Untested! Remade all the code from the old script. Needs to be tested first. Does not cause error on startup, that much ik

--- escapeplan.lua
+++ escapeplan.lua
@@ -35,31 +35,63 @@ end
 function OnPlayerEscapeButDead(plr,_,before) --make them actually escape
 	setplayertype(plr,before)
 	escapedplrs[plr] = true
-	if before == 3 then setplayerposition(plr,"gatea", escape2[0], escape2[1], escape2[2]) else setplayerposition(plr,"exit1", escape1[0], escape1[1], escape1[2]) end
+	if before == 3 then setplayerposition(plr,"gatea", escape2[1], escape2[2], escape2[3]) else setplayerposition(plr,"exit1", escape1[1], escape1[2], escape1[3]) end
+    return -1
 end
 
 function escaped(plr,before)
     if not escapedplrs[plr] then return end
     escapedplrs = false --Remove them from escapedplrs list
-    if before == 7 then setplayerposition(plr,"exit1", exit1[0], exit1[1], exit1[2]) else setplayerposition(plr,"gatea", exit2[0], exit2[1], exit2[2]) end
+    if before == 7 then setplayerposition(plr,"exit1", exit1[1], exit1[2], exit1[3]) else setplayerposition(plr,"gatea", exit2[1], exit2[2], exit2[3]) end
     --If they're on the list and turns into Chaos, then they escaped tho gate b. Otherwise they must have escaped tho gate a.
 end
 
-function OnPlayerEscape(plr,before) CreateTimer("escaped",100,0,plr,before) end --execute escaped function after 100 milliseconds to make room for the natural (delayed) spawn position change
+function OnPlayerEscape(plr,before) createtimer("escaped",100,0,plr,before); return -1 end --execute escaped function after 100 milliseconds to make room for the natural (delayed) spawn position change
 
 function OnPlayerCuffPlayer(_,plr)
-    if GetPlayerHandcuff(plr) == 0 then	return end
+    if getplayerhandcuff(plr) == 0 then	return -1 end
     local role = getplayertype(plr)
-    room = GetRoomName(GetPlayerRoomID(plr))
-    local plrentity = GetPlayerEntity(plr)
+    room = getroomname(getplayerroomid(plr))
+    local plrentity = getplayerentity(plr)
     local plrposition = {entityx(plrentity),entityy(plrentity),entityz(plrentity)}
-    if room == "exit1" and role != 3 and plrposition[1] >= (escape1[0] - 2) and plrposition[3] <= (escape1[2] + 10) then --if handcuffed SCPF staff then be sure to become CI
+    if room == "exit1" and role ~= 3 and (plrposition[1] >= escape1[1] - 2) and (plrposition[3] <= escape1[3] + 10) then --if handcuffed SCPF staff then be sure to become CI
         escapedplrs[plr] = true
-        setplayerposition(plr,"gatea", escape2[0], escape2[1], escape2[2])
+        setplayerposition(plr,"gatea", escape2[1], escape2[2], escape2[3])
     end
     if room == "gatea" and role == 3 and plrposition[1] >= 118 and plrposition[2] <= 496 and plrposition[3] <= 20 then --if handcuffed CD then MTF
         escapedplrs[plr] = true
-        setplayerposition(plr,"exit1", escape1[0], escape1[1], escape1[2])
+        setplayerposition(plr,"exit1", escape1[1], escape1[2], escape1[3])
     end
     createtimer("OnPlayerCuffPlayer",1000,0,_,plr) --script needs to run every 1 second to detect. It takes >1 second from the beginning of new escape coords to reach proper
+    return -1
+end
+
+function OnPlayerConsole(plr,txt)
+    if txt = "handcuff" then
+        if getplayerhandcuff(plr) == 1 then
+            setplayerhandcuff(plr,0)
+        else
+            setplayerhandcuff(plr,1)
+            OnPlayerCuffPlayer(0,plr) --Call cuff callback. Assume player 0 == server
+        end
+elseif txt == "escape" then
+        local role = getplayertype(plr)
+        if role == 3 then
+            setplayerposition(plr,"gatea", escape2[1], escape2[2], escape2[3])
+        elseif role == 4 or role == 8 then
+            setplayerposition(plr,"exit1", escape1[1], escape1[2], escape1[3])
+        else
+            sendmessage(plr,"You are not an Escape Class Role")
+        end
+    elseif txt == "testescape" then
+        local role = getplayertype(plr)
+        if role == 3 then
+            setplayerposition(plr,"exit1", escape1[1], escape1[2], escape1[3])
+        elseif role == 4 or role == 8 then
+            setplayerposition(plr,"gatea", escape2[1], escape2[2], escape2[3])            
+        else
+            sendmessage(plr,"You are not an Escape Class Role")
+        end
+    end
+    return -1
 end
