1667b82ec21307bcf4ac8dbaf25e8e03ad75d622
Goodman <arafezazgaminglegend@gmail.com>
2022-11-04 12:05:19 +1100

    Tested and fixed regular escapes. Now to test cuffed escapes

--- escapeplan.lua
+++ escapeplan.lua
@@ -28,35 +28,37 @@ function escapecoords()
         }    
         if type(select[getroomname(i)]) == "function" then select[getroomname(i)]() end
         if entitypointers[1] and entitypointers[2] and entitypointers[3] then --if all coords found, end function
-            escape2f = function() setplayerposition(plr,"gatea", escape2[1], escape2[2], escape2[3]) end --escape2f() will now teleport plr to gate a escape
-            escape1f = function() setplayerposition(plr,"exit1", escape1[1], escape1[2], escape1[3]) end --escape2f() will now teleport plr to gate b escape
+            escape2f = function(plr) setplayerposition(plr,"gatea", escape2[1], escape2[2], escape2[3]) end --escape2f(plr) will now teleport plr to gate a escape
+            escape1f = function(plr) setplayerposition(plr,"exit1", escape1[1], escape1[2], escape1[3]) end --escape2f(plr) will now teleport plr to gate b escape
             return false --return false so script doesnt restart server
         end
     end
     return true
 end
 
-function OnPlayerEscapeButDead(plr,_,before) --make them actually escape
-	setplayertype(plr,before)
+function OnPlayerEscapeButDead(plr,_,role) --make them actually escape
+	setplayertype(plr,role)
+    print("lego")
 	escapedplrs[plr] = true
-	if before == 3 then escape2f() else escape2f() end
+	if role == 3 then escape2f(plr) else escape1f(plr) end
     return -1
 end
 
-function escaped(plr,before)
-    print(os.time().."plr")
+function escaped(plr,role)
+    plr,role = tonumber(plr), tonumber(role)
     if escapedplrs[plr] then
-        print("mango")
+        print("pain")
         escapedplrs[plr] = false --Remove them from escapedplrs list
-        if before == 7 then setplayerposition(plr,"exit1", exit1[1], exit1[2], exit1[3]) else setplayerposition(plr,"gatea", exit2[1], exit2[2], exit2[3]) end
+        if role == 7 then setplayerposition(plr,"exit1", exit1[1], exit1[2]+10, exit1[3]) else setplayerposition(plr,"gatea", exit2[1], exit2[2], exit2[3]) end
         --If they're on the list and turns into Chaos, then they escaped tho gate b. Otherwise they must have escaped tho gate a.
     end
     return -1
 end
 
-function OnPlayerEscape(plr,before);print(os.time()); createtimer("escaped",5000,0,plr,before); print(plr); return -1 end --execute escaped function after 100 milliseconds to make room for the natural (delayed) spawn position change
+function OnPlayerEscape(plr,role) createtimer("escaped",50,0,plr,role) return -1 end --execute escaped function after 100 milliseconds to make room for the natural (delayed) spawn position change
 
 function OnPlayerCuffPlayer(_,plr) --For cuffed players to join enemy team even if escape tho own gate
+    plr = tonumber(plr)
     if getplayerhandcuff(plr) == 0 then	return -1 end
     local role = getplayertype(plr)
     room = getroomname(getplayerroomid(plr))
@@ -64,11 +66,11 @@ function OnPlayerCuffPlayer(_,plr) --For cuffed players to join enemy team even 
     local plrposition = {entityx(plrentity),entityy(plrentity),entityz(plrentity)}
     if room == "exit1" and role ~= 3 and (plrposition[1] >= escape1[1] - 2) and (plrposition[3] <= escape1[3] + 10) then --if handcuffed SCPF staff then be sure to become CI
         escapedplrs[plr] = true
-        escape2f()
+        escape1f(plr)
     end
     if room == "gatea" and role == 3 and plrposition[1] >= 118 and plrposition[2] <= 496 and plrposition[3] <= 20 then --if handcuffed CD then MTF
         escapedplrs[plr] = true
-        escape2f()
+        escape2f(plr)
     end
     createtimer("OnPlayerCuffPlayer",1000,0,_,plr) --script needs to run every 1 second to detect. It takes >1 second from the beginning of new escape coords to reach proper
     return -1
@@ -90,8 +92,8 @@ function OnPlayerConsole(plr,txt) --Console makes testing a lot easier
                 OnPlayerCuffPlayer(0,plr) --Call cuff callback. Assume player 0 == server
             end
         end,
-        ["escape"] = function() teleport(function() escape2f() end, function() escape1f() end) end, --Functions as arguements. Runs when called inside the teleport()
-        ["testescape"] = function() teleport(function() escape1f() end, function() escape2f() end) end
+        ["escape"] = function() teleport(function() escape2f(plr) end, function() escape1f(plr) end) end, --Functions as arguements. Runs when called inside the teleport()
+        ["testescape"] = function() teleport(function() escape1f(plr) end, function() escape2f(plr) end) end
     }
     if type(select[txt]) == "function" then select[txt]() end
     return -1
